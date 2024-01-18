dbb5416a2dda23b42d68c4956db914ffc2c24e96
Vikas Kumar <vikas_063@hotmail.com>
2024-01-18 17:10:47 +0530

    Initial Commit

---
+++ ProjectTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectTemplate
+//
+//  Created by Vikas Kumar on 18/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
