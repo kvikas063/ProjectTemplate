8623770b3edc7495afc5ec2d8e613598121e554f
Vikas Kumar <vikas_063@hotmail.com>
2024-01-30 19:28:15 +0530

    Added Corner type for View Extension

--- ProjectTemplate/Helper/Extension/View+Ext.swift
+++ ProjectTemplate/Helper/Extension/View+Ext.swift
@@ -225,3 +225,36 @@ extension UIView {
         return self
     }
 }
+
+enum RoundType {
+    case top
+    case none
+    case bottom
+    case both
+}
+
+extension UIView {
+
+    func round(with type: RoundType, radius: CGFloat = 3.0) {
+        var corners: UIRectCorner
+
+        switch type {
+        case .top:
+            corners = [.topLeft, .topRight]
+        case .none:
+            corners = []
+        case .bottom:
+            corners = [.bottomLeft, .bottomRight]
+        case .both:
+            corners = [.allCorners]
+        }
+
+        DispatchQueue.main.async {
+            let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            self.layer.mask = mask
+        }
+    }
+
+}
